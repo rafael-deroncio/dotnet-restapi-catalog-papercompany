75d36687334a0cd78eb1d148f63a9927f09dea4b
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-05-15 16:02:47 -0300

    Add controllers

---
+++ src/PapperCompany.Catalog.API/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PapperCompany.Catalog.API.COntrollers;
+
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiVersion("1.0")]
+[ApiController]
+[Authorize]
+public class CategoryController : Controller
+{
+    public CategoryController()
+    {
+
+    }
+
+    [HttpGet]
+    [AllowAnonymous]
+    public async Task<IActionResult> GetCategories() => Ok(await Task.FromResult(0));
+
+    [HttpGet("{id:int}")]
+    [AllowAnonymous]
+    public async Task<IActionResult> GetCategory(int id) => Ok(await Task.FromResult(0));
+
+    [HttpPost]
+    [Authorize(Roles = "Admin, Manager")]
+    public async Task<IActionResult> PostCategory() => Ok(await Task.FromResult(0));
+
+    [HttpPut("{id:int}")]
+    [Authorize(Roles = "Admin, Manager")]
+    public async Task<IActionResult> PutCategory(int id) => Ok(await Task.FromResult(0));
+
+    [HttpDelete("{id:int}")]
+    [Authorize(Roles = "Admin, Manager")]
+    public async Task<IActionResult> DeleteCategory(int id) => Ok(await Task.FromResult(0));
+}
---
+++ src/PapperCompany.Catalog.API/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PapperCompany.Catalog.API.COntrollers;
+
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiVersion("1.0")]
+[ApiController]
+[Authorize]
+public class ProductController : Controller
+{
+    public ProductController()
+    {
+
+    }
+
+    [HttpGet]
+    [AllowAnonymous]
+    public async Task<IActionResult> GetProducts() => Ok(await Task.FromResult(0));
+
+    [HttpGet("{id:int}")]
+    [AllowAnonymous]
+    public async Task<IActionResult> GetProduct(int id) => Ok(await Task.FromResult(0));
+
+    [HttpPost]
+    [Authorize(Roles = "Admin, Manager")]
+    public async Task<IActionResult> PostProduct() => Ok(await Task.FromResult(0));
+
+    [HttpPut("{id:int}")]
+    [Authorize(Roles = "Admin, Manager")]
+    public async Task<IActionResult> PutProduct(int id) => Ok(await Task.FromResult(0));
+
+    [HttpDelete("{id:int}")]
+    [Authorize(Roles = "Admin, Manager")]
+    public async Task<IActionResult> DeleteProduct(int id) => Ok(await Task.FromResult(0));
+}

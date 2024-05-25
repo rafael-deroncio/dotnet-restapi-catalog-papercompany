65254889752bd4da72b4681973a927fc252fcc2b
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-05-24 23:46:24 -0300

    Add Category Tests

---
+++ src/PapperCompany.Catalog.Test/Services/CategoryServiceTest.cs
@@ -0,0 +1,46 @@
+﻿namespace PapperCompany.Catalog.Test;
+
+public class CategoryServiceTest
+{
+    [Fact]
+    public async Task MustGetCategorySuccessfully()
+    {
+        await Task.FromResult(0);
+    }
+
+    [Fact]
+    public async Task MustGetCategoriesSuccessfully()
+    {
+        await Task.FromResult(0);
+    }
+
+    [Fact]
+    public async Task MustCreateNewCategorySuccessfully()
+    {
+        await Task.FromResult(0);
+    }
+
+    [Fact]
+    public async Task MustEditCategorySuccessfully()
+    {
+        await Task.FromResult(0);
+    }
+
+    [Fact]
+    public async Task MustDeleteCategorySuccessfully()
+    {
+        await Task.FromResult(0);
+    }
+
+    [Fact]
+    public async Task MustReturnBadRequest()
+    {
+        await Task.FromResult(0);
+    }
+
+    [Fact]
+    public async Task MustReturnNotFound()
+    {
+        await Task.FromResult(0);
+    }
+}

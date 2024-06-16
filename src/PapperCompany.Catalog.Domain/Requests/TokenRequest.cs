2b07148f574469f0c6bfe471ac9dc6a4e0f721d3
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-06-16 15:31:27 -0300

    add token request e response

---
+++ src/PapperCompany.Catalog.Domain/Requests/TokenRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace PapperCompany.Catalog.Domain;
+
+public class TokenRequest
+{
+    [JsonIgnore]
+    public static string RequestId => Guid.NewGuid().ToString();
+
+    [Required(ErrorMessage = "The user field is required")]
+    public string Username { get; set; }
+
+    [Required(ErrorMessage = "The user field is required")]
+    public string Role { get; set; }
+
+    [Required(ErrorMessage = "The user field is required")]
+    public IEnumerable<string> Claims { get; set; }
+
+}
---
+++ src/PapperCompany.Catalog.Domain/Responses/TokenResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace PapperCompany.Catalog.Domain;
+
+public class TokenResponse
+{
+    public string Token { get; set; }
+    public DateTime Expires { get; set; }
+    public string Message { get; set; }
+}
